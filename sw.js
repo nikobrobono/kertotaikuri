5768cde4fd1b06de5808b6d6b28f42ad594d43e5
nikobrobono <niko.tamsi@gmail.com>
2026-08-19 19:37:19 +0300

    Activate PWA updates immediately

--- sw.js
+++ sw.js
@@ -1,7 +1,13 @@
-const CACHE = "kertotaikuri-v2";
+const CACHE = "kertotaikuri-v3";
 const ASSETS = ["./", "./index.html", "./manifest.webmanifest", "./icons/icon.svg", "./src/styles.css", "./src/main.js", "./src/domain/curriculum.js", "./src/domain/question.js", "./src/domain/hints.js", "./src/domain/finaleScoring.js", "./src/data/progressStore.js"];
-self.addEventListener("install", (event) => event.waitUntil(caches.open(CACHE).then((cache) => cache.addAll(ASSETS))));
-self.addEventListener("activate", (event) => event.waitUntil(caches.keys().then((keys) => Promise.all(keys.filter((key) => key !== CACHE).map((key) => caches.delete(key))))));
+self.addEventListener("install", (event) => event.waitUntil(Promise.all([
+  self.skipWaiting(),
+  caches.open(CACHE).then((cache) => cache.addAll(ASSETS))
+])));
+self.addEventListener("activate", (event) => event.waitUntil(Promise.all([
+  self.clients.claim(),
+  caches.keys().then((keys) => Promise.all(keys.filter((key) => key !== CACHE).map((key) => caches.delete(key))))
+])));
 self.addEventListener("fetch", (event) => {
   if (event.request.method !== "GET") return;
   event.respondWith(fetch(event.request).then((response) => {
